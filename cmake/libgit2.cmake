43b22825d01e0238a6e40a13244c93bbd18c4304
Force Charlie <ipvb@qq.com>
2018-08-07 10:20:58 +0800

    libgit2 use ExternalProject_Add

---
+++ cmake/libgit2.cmake
@@ -0,0 +1,9 @@
+option(SONAME "" OFF)
+option(USE_SSH "" OFF)
+option(USE_HTTPS "" OFF)
+option(USE_GSSAPI "" OFF)
+option(BUILD_CLAR "Build clar" OFF)
+option(ENABLE_TEST "Build Test" OFF)
+install(FILES COPYING
+  DESTINATION share/libgit2
+)
